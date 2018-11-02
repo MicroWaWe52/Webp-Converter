55f1853314e2baef38dfe078775122f12773773e
marco spiga <marcospiga52@yahoo.it>
2018-11-02 13:38:18 +0100

    Aggiungere i file di progetto.

---
+++ WebP Converter/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WebP_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialogExe = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialogImg = new System.Windows.Forms.OpenFileDialog();
+            this.textBoxExe = new System.Windows.Forms.TextBox();
+            this.textBoxImg = new System.Windows.Forms.TextBox();
+            this.buttonExe = new System.Windows.Forms.Button();
+            this.buttonImg = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBoxImg = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialogExe
+            // 
+            this.openFileDialogExe.FileName = "openFileDialog1";
+            // 
+            // openFileDialogImg
+            // 
+            this.openFileDialogImg.FileName = "openFileDialog2";
+            // 
+            // textBoxExe
+            // 
+            this.textBoxExe.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxExe.Location = new System.Drawing.Point(12, 25);
+            this.textBoxExe.Name = "textBoxExe";
+            this.textBoxExe.Size = new System.Drawing.Size(394, 20);
+            this.textBoxExe.TabIndex = 0;
+            // 
+            // textBoxImg
+            // 
+            this.textBoxImg.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxImg.Location = new System.Drawing.Point(12, 64);
+            this.textBoxImg.Name = "textBoxImg";
+            this.textBoxImg.Size = new System.Drawing.Size(394, 20);
+            this.textBoxImg.TabIndex = 1;
+            // 
+            // buttonExe
+            // 
+            this.buttonExe.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonExe.Location = new System.Drawing.Point(412, 25);
+            this.buttonExe.Name = "buttonExe";
+            this.buttonExe.Size = new System.Drawing.Size(75, 23);
+            this.buttonExe.TabIndex = 2;
+            this.buttonExe.Text = "Select...";
+            this.buttonExe.UseVisualStyleBackColor = true;
+            this.buttonExe.Click += new System.EventHandler(this.buttonExe_Click);
+            // 
+            // buttonImg
+            // 
+            this.buttonImg.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonImg.Location = new System.Drawing.Point(412, 64);
+            this.buttonImg.Name = "buttonImg";
+            this.buttonImg.Size = new System.Drawing.Size(75, 23);
+            this.buttonImg.TabIndex = 3;
+            this.buttonImg.Text = "Select...";
+            this.buttonImg.UseVisualStyleBackColor = true;
+            this.buttonImg.Click += new System.EventHandler(this.buttonImg_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "cwebp.exe Executable path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Image path";
+            // 
+            // pictureBoxImg
+            // 
+            this.pictureBoxImg.Location = new System.Drawing.Point(12, 90);
+            this.pictureBoxImg.Name = "pictureBoxImg";
+            this.pictureBoxImg.Size = new System.Drawing.Size(154, 73);
+            this.pictureBoxImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxImg.TabIndex = 6;
+            this.pictureBoxImg.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(412, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 73);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Convert!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 175);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBoxImg);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonImg);
+            this.Controls.Add(this.buttonExe);
+            this.Controls.Add(this.textBoxImg);
+            this.Controls.Add(this.textBoxExe);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialogExe;
+        private System.Windows.Forms.OpenFileDialog openFileDialogImg;
+        private System.Windows.Forms.TextBox textBoxExe;
+        private System.Windows.Forms.TextBox textBoxImg;
+        private System.Windows.Forms.Button buttonExe;
+        private System.Windows.Forms.Button buttonImg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBoxImg;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WebP Converter/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WebP_Converter
+{
+   
+    public partial class Form1 : Form
+    {
+        string exePath;
+        string imgPath;
+        string imgName;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExe_Click(object sender, EventArgs e)
+        {
+            if (openFileDialogExe.ShowDialog() == DialogResult.OK)
+            {
+                textBoxExe.Text = openFileDialogExe.FileName;
+                exePath = openFileDialogExe.FileName;
+                Properties.Settings.Default.exePath = exePath;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            exePath = Properties.Settings.Default.exePath;
+            textBoxExe.Text = exePath;
+            openFileDialogExe.InitialDirectory = exePath;
+            buttonExe.Focus();
+        }
+
+        private void buttonImg_Click(object sender, EventArgs e)
+        {
+            if (openFileDialogImg.ShowDialog() == DialogResult.OK)
+            {
+                textBoxImg.Text = openFileDialogImg.FileName;
+                imgPath = openFileDialogImg.FileName;
+                imgName = Path.GetFileNameWithoutExtension(openFileDialogImg.FileName);
+                pictureBoxImg.ImageLocation = imgPath;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ExecuteCommand($"-q 100 {imgPath} -o {imgName}.webp");
+        }
+        public void ExecuteCommand(string command)
+        {
+            try
+            {
+                Process.Start(exePath, command);
+                var finalPath = Path.GetDirectoryName(exePath);
+                File.Move($"{imgName}.webp", finalPath + $"\\{imgName}.webp");
+                MessageBox.Show($".webp File saved in{finalPath}");
+
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
+    }
+}
